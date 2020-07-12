a874b87d0d7df962193135a9ceae479633bae34b
Andrew <48335990+espresso64@users.noreply.github.com>
2020-07-12 15:43:56 -0700

    testing IDE push

--- src/main/java/cst438/Cst438w3AApplication.java
+++ src/main/java/cst438/Cst438w3AApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Cst438w3AApplication {
-
+//Test
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Cst438w3AApplication.class, args);
 	}
